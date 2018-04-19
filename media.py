7e8bfbbac4a2c1c0f5b4c724ce9750587dc34a44
Jbrooks1200 <36597246+Jbrooks1200@users.noreply.github.com>
2018-04-19 11:37:30 -0400

    Update media.py

--- media.py
+++ media.py
@@ -4,7 +4,7 @@ class Movie():
 	"""testing out the doc pre-defined variable"""
 
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"] # class variables that do not change should be done in all caps.
-
+	#function that initializes inputs
 	def __init__ (self, movie_title, movie_storyline, poster_image, trail_youtube, rating):
 		self.title = movie_title
 		self.storyline = movie_storyline
@@ -13,9 +13,9 @@ class Movie():
 		self.rating = rating
 		
 
-	def show_trailer(self):
+	def show_trailer(self):#function to show movie trailer
 		webbrowser.open(self.trailer_youtube_url)
-	def show_poster(self):
+	def show_poster(self):#function that produces the movie cover art
 		webbrowser.open(self.poster_image_url)
 
 
